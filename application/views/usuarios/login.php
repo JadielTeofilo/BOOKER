749112e1fbc8d7514d643363160532db3e353b54
luhansen <luluka.hansen@gmail.com>
2016-11-05 20:46:06 -0200

    finalmente tela bonita

--- application/views/usuarios/login.php
+++ application/views/usuarios/login.php
@@ -1,6 +1,6 @@
 <?php echo $this->load->view('_inc/header') ?>
 
-<body class="valign-wrapper" >
+<body>
      <div id="titulo">
         <space>
             <a href="LAH.html">BOOKERS</a>
@@ -18,33 +18,29 @@
     <div class="jumbotron">
       <div class="container">
         <div class ="row">
-        <div class="col-md-8">
+        <div class="col s6">
             <h1 class="thin">BOOKERS</h1>
             <p class="flow-text">Sua biblioteca virtual, feita pra voce, por voce</p>
             <p class="flow-text">Faca seu login e acesse seus livros favoritos</p>
         </div>
-        <div id="logingeral" class="col-md-4" >
+        <div id="logingeral" class="col s6" >
+        <br><br><br>
            <?php $attributes = array('class' => 'center-align');?>
             <?php echo form_open('index.php/usuario/logar', $attributes)?>
                 <div class="row" >
                     <div class="center-align" class="input-field col s3">
                         <input required id="login" type="text" placeholder="Usuário" name="login" autofocus>
-                        <label for="email">Nome Usuário</label>
                     </div>
                 </div>
                 <div class="row">
                     <div class="center-align" class="input-field s3">
                         <input required id ="password" type="password" placeholder="Senha" name="senha" autofocus>
-                        <label for="password">Password</label>
                     </div>
                 </div>
                 <button class="btn waves-effect waves-light" type="submit" name="enviar">Submit
                     <i class="material-icons right">send</i>
                 </button>
         <?php echo form_close() ?><br>
-            <div class="row">
-                <a href="<?php echo base_url().'index.php/usuario/senha_perdida1'?>">Esqueci minha senha</a>
-            </div>
         </div>
         </div>
       </div>
