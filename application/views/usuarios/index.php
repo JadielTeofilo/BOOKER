d915fbf5f24db556b3960b07a4b8ded32f1fbe77
luhansen <luluka.hansen@gmail.com>
2016-11-06 17:36:00 -0200

    deixar mais bonito

--- application/views/usuarios/index.php
+++ application/views/usuarios/index.php
@@ -1,21 +1,23 @@
 <?php echo $this->load->view('_inc/header') ?>
-<body>
-    <div id="titulo">
-        <space>
-            <a href="LAH.html">BOOKERS</a>
-        </space>
-    </div>
 
-    <div class="nav">
-        <div class="row">
-            <ul class= "col s6 offset-s0">
-                <li><a href="<?php echo base_url().'index.php/usuario/mostrarlogar'?>">Cadastrar</a></li>
-                <li><a href="<?php echo base_url().'index.php/usuario/mostrarlogar'?>">Entrar</a></li>
+<!--Menu Superior-->
+<div class = "navbar-fixed">
+    <nav class="grey">
+        <div class="nav-wrapper">
+            <a href="<?php echo base_url().'index.php/usuario/home_low_lv' ?>" class="center brand-logo">BOOKERS</a>
+            <ul class="left hide-on-med-and-down">
+                <li>
+                    <a href="<?php echo base_url().'index.php/usuario/mostrarlogar'?>">Cadastrar</a>
+                </li>
+                <li>
+                    <a href="<?php echo base_url().'index.php/usuario/mostrarlogar'?>">Entrar</a>
+                </li>
             </ul>
         </div>
-    </div>
+    </nav>
+</div>
 
-    <div class="jumbotron row valign-wrapper">
+ <div class="jumbotron row valign-wrapper">
       <div class="vallign col s6 offset-s0">
         <h1 class="thin">BOOKERS</h1>
         <p class="flow-text">Sua biblioteca virtual, feita pra voce, por voce</p>
--- application/views/usuarios/login.php
+++ application/views/usuarios/login.php
@@ -2,28 +2,28 @@
 
 
 <body>
-     <div id="titulo">
-        <space>
-            <a href="LAH.html">BOOKERS</a>
-        </space>
-    </div>
-
-    <div class="nav">
-        <div class="row">
-            <ul class= "pull-right">
-                <li><a href="<?php echo base_url().'index.php/usuario/mostrarlogar'?>">Cadastrar</a></li>
-                <li><a href="<?php echo base_url().'index.php/usuario/mostrarlogar'?>">Entrar</a></li>
+   <div class = "navbar-fixed">
+    <nav class="grey">
+        <div class="nav-wrapper">
+            <a href="<?php echo base_url().'index.php/usuario/home_low_lv' ?>" class="center brand-logo">BOOKERS</a>
+            <ul class="left hide-on-med-and-down">
+                <li>
+                    <a href="<?php echo base_url().'index.php/usuario/mostrarlogar'?>">Cadastrar</a>
+                </li>
+                <li>
+                    <a href="<?php echo base_url().'index.php/usuario/mostrarlogar'?>">Entrar</a>
+                </li>
             </ul>
         </div>
-    </div>
-    <div class="jumbotron">
-      <div class="container">
-        <div class ="row">
-        <div class="col s6">
-            <h1 class="thin">BOOKERS</h1>
-            <p class="flow-text">Sua biblioteca virtual, feita pra voce, por voce</p>
-            <p class="flow-text">Faca seu login e acesse seus livros favoritos</p>
-        </div>
+    </nav>
+</div>
+
+ <div class="jumbotron row valign-wrapper">
+      <div class="vallign col s6 offset-s0">
+        <h1 class="thin">BOOKERS</h1>
+        <p class="flow-text">Sua biblioteca virtual, feita pra voce, por voce</p>
+        <p class="flow-text">Faca seu login e acesse seus livros favoritos</p>
+      </div>
         <div id="logingeral" class="col s6" >
         <br><br><br>
            <?php $attributes = array('class' => 'center-align');?>
